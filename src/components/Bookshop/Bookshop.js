65cb9add3cc4cde6b5191c245b1d321cb88cd0b9
git-mhasan <mahadi2020dev@gmail.com>
2022-03-26 00:18:26 +0600

    data call activated

--- src/components/Bookshop/Bookshop.js
+++ src/components/Bookshop/Bookshop.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Bookshop.css';
 
 const Bookshop = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('books.JSON')
+            .then(res => res.json())
+            .then(data => setBooks(data));
+    }, []);
+
     return (
-        <div className='book-shop'>
-            <h2>The Baffled Bookworm</h2>
+        <div>
+            <h2 className='header-title'>The Baffled Bookworm</h2>
+            <div className='book-shop'>
+                <div className='book-items'>
+                    {books[1].name}
+                </div>
+                <div className='book-cart'>
+                    <h1>hhhh</h1>
+                </div>
+            </div>
         </div>
     );
 };
